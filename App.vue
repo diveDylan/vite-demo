3b36335f7266080e39ed02e0a8c8a62b054acda2
dylan <dylan_zeng92@qq.com>
2020-05-12 16:38:41 +0800

    init

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <WaterMark />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import WaterMark from './components/WaterMark.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    WaterMark
+  }
+}
+</script>
